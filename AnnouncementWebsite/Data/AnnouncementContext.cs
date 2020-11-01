52882b44f3eade9240d208a4d841b351baecb7d8
Yurii <fytruk14@gmail.com>
2020-11-01 16:00:40 +0200

    announcement website

---
+++ AnnouncementWebsite/Data/AnnouncementContext.cs
@@ -0,0 +1,15 @@
+﻿using AnnouncementWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnouncementWebsite.Data
+{
+    public class AnnouncementContext : DbContext
+    {
+        public AnnouncementContext(DbContextOptions<AnnouncementContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Announcement> Announcements { get; set; }
+    }
+}
---
+++ AnnouncementWebsite/ViewModels/AnnouncementsListingModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using AnnouncementWebsite.Models;
+
+namespace AnnouncementWebsite.ViewModels
+{
+    public class AnnouncementsListingModel
+    {
+        public IEnumerable<Announcement> Announcements { get; set; }
+        public string SearchTerm { get; set; }
+    }
+}
